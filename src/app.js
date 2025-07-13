4916b0b221d5821cb9121fc8f5e2a3256b634e5b
Sk Mursalin <skmursalin02@gmail.com>
2025-07-13 11:35:40 +0530

    explore the route in express

--- src/app.js
+++ src/app.js
@@ -2,9 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use("/server",(req,res)=>{
-    res.send("helo from server devv")
+app.get("/server",(req,res)=>{
+    res.send("helo from get  server devv")
+}); 
+
+app.post("/server",(req,res)=>{
+    res.send("crete a instance in db ")
 });
+
+app.get("/param/:userId",(req,res)=>{
+    console.log(req.params);
+    res.send("testing multiple route")
+})
+
+
 app.listen(3000,()=>{
     console.log("server is runnnig on port 3000..");
 });
