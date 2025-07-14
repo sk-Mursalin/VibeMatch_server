0d1163b72ad4709eda7d424540fbd29ee54d81f6
Sk Mursalin <skmursalin02@gmail.com>
2025-07-14 11:45:11 +0530

    writing middleware

--- src/app.js
+++ src/app.js
@@ -2,20 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.get("/server",(req,res)=>{
-    res.send("helo from get  server devv")
-}); 
-
-app.post("/server",(req,res)=>{
-    res.send("crete a instance in db ")
+app.use("/admin", (req, res,next) => {
+    console.log("auth checking");
+    let token = "xyz";
+    let isAdmin = token === "xyz"
+    if (!isAdmin) {
+        res.status(401).send("admin is not authorised")
+    } else {
+        next()
+    }
 });
 
-app.get("/param/:userId",(req,res)=>{
-    console.log(req.params);
-    res.send("testing multiple route")
-})
+app.get("/admin/getAllData", (req, res) => {
+    res.send("send all data to admin")
+});
 
+app.get("/admin/deleteAllData", (req, res) => {
+    res.send("delete all data of admin")
+});
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("server is runnnig on port 3000..");
 });
