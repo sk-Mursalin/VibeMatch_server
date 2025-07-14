3ef69959d17e60b5eba4ca4c8ed8bbe21469a9c7
Sk Mursalin <skmursalin02@gmail.com>
2025-07-14 12:25:15 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -2,23 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use("/admin", (req, res,next) => {
-    console.log("auth checking");
-    let token = "xyz";
-    let isAdmin = token === "xyz"
-    if (!isAdmin) {
-        res.status(401).send("admin is not authorised")
-    } else {
-        next()
+app.get("/route", (req, res) => {
+    throw new Error("sjfnw");
+});
+
+app.use("/", (err, req, res) => {
+    if (err) {
+        res.status(500).send("error is occured")
     }
-});
-
-app.get("/admin/getAllData", (req, res) => {
-    res.send("send all data to admin")
-});
-
-app.get("/admin/deleteAllData", (req, res) => {
-    res.send("delete all data of admin")
 });
 
 app.listen(3000, () => {
