d47797bfc407afd0e0dc2eab6241aadae81fc50e
Sk Mursalin <skmursalin02@gmail.com>
2025-07-15 10:20:50 +0530

    create signup api

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 const express = require("express");
+const {databaseConnection} = require("./config/dataBase");
+const User = require("./model/user");
 
 const app = express();
+app.use(express.json());
 
-app.get("/route", (req, res) => {
-    throw new Error("sjfnw");
-});
-
-app.use("/", (err, req, res) => {
-    if (err) {
-        res.status(500).send("error is occured")
+app.post("/signup",async (req,res)=>{
+    const user = new User(req.body);
+    try{
+       await  user.save()
+       res.send("user successfully created")
+    }
+    catch(err){
+        res.status(400).send("error occured")
     }
 });
 
-app.listen(3000, () => {
-    console.log("server is runnnig on port 3000..");
-});
+databaseConnection().then(() => {
+    console.log("database connection is establish");
+    app.listen(3000, () => {
+        console.log("server is runnnig on port 3000..");
+    });
+}).catch((err) => {
+    console.log("a error occured to connect db");
+})
+
---
+++ src/config/dataBase.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const databaseConnection  = async()=>{
+    await mongoose.connect("mongodb+srv://skmursalin02:9378496506@learnmongo.lhejvp7.mongodb.net/devTinder");
+}
+
+module.exports = {databaseConnection};
