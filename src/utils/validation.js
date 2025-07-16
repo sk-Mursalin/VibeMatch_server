263a6e3cb0e46d2893bd1cc686c12881057959c0
Sk Mursalin <skmursalin02@gmail.com>
2025-07-16 16:27:42 +0530

    log in api

---
+++ src/utils/validation.js
@@ -0,0 +1,15 @@
+const validator = require("validator");
+const signupValidation = (req) => {
+    const { firstName, lastName, email, password } = req.body;
+
+    if (!validator.isEmail(email)) {
+        throw new Error("please enter a valid email")
+    }
+    else if (firstName.length >= 10 || !lastName.length >= 50) {
+        throw new Error("invalid")
+    }
+}
+
+module.exports = {
+    signupValidation
+}
