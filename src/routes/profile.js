b2db25c3488d3bcdb397feea18881cc4a3f46955
Sk Mursalin <skmursalin02@gmail.com>
2025-12-14 10:52:19 +0530

    .

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -61,20 +61,6 @@ profileRouter.get("/profile/get/:ProfileId", userAuth, async (req, res) => {
     }
 });
 
-profileRouter.delete("/profile/delete", userAuth, async (req, res) => {
-    const { email, _id } = req.profile
-    console.log(req.profile);
-    const request = await connectionRequestModel.find({ fromUserId: _id })
-    console.log(request);
-    // const { acknowledged } = await user.deleteOne({ email: email })
 
-    // if (!acknowledged) {
-    //     return res.status(500).json({ data: "server error" })
-    // }
-    // else {
-    //     return res.status(200).json({ data: "account deleted successfully" })
-
-    // }
-})
 
 module.exports = profileRouter;
